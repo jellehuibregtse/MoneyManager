c6cadd8446b303bd4bc503022108fb46ca39bf6f
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-06-03 11:28:40 +0200

    Nullable

--- MoneyManager.Domain/Models/Transaction.cs
+++ MoneyManager.Domain/Models/Transaction.cs
@@ -8,7 +8,7 @@ namespace MoneyManager.Models
     {
         public int Id { get; set; }
         public int AccountId { get; set; }
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
         [StringLength(50, MinimumLength = 3)] public string Name { get; set; }
         [Column(TypeName = "decimal(18,2)")]
         [DataType(DataType.Currency)]
