63dcfcb320cf3abba29f58b80f01b8a46f54d6b9
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2020-06-15 15:06:30 +0200

    Updated Tests

---
+++ MoneyManager.Tests/AccountControllerTests.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MoneyManager.Web;
+using Xunit;
+
+namespace MoneyManager.IntegrationTests
+{
+    public class AccountControllerTests : IClassFixture<MoneyManagerWebFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public AccountControllerTests(MoneyManagerWebFactory<Startup> factory)
+        {
+            var projectDirectory = Directory.GetCurrentDirectory();
+            var configurationPath = Path.Combine(projectDirectory, "appsettings.json");
+
+            _factory = factory.WithWebHostBuilder(builder =>
+            {
+                builder.UseSolutionRelativeContentRoot(projectDirectory);
+
+                builder.ConfigureAppConfiguration((context, configuration) =>
+                {
+                    configuration.AddJsonFile(configurationPath);
+                });
+
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddMvc().AddApplicationPart(typeof(Startup).Assembly);
+                });
+            });
+        }
+
+        /// <summary>
+        /// This tests verifies that all URLs given end with response code in range (200-299); It makes sure that
+        /// requests don't fail and return content type text/html. 
+        /// </summary>
+        /// <param name="url">The URL which is being tested.</param>
+        /// <returns></returns>
+        [Theory]
+        [InlineData("/Account")]
+        [InlineData("/Account/Index")]
+        [InlineData("/Account/Create")]
+        [InlineData("/Account/Details")]
+        [InlineData("/Account/Edit")]
+        [InlineData("/Account/Delete")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+        [Theory]
+        [InlineData("/")]
+        public async Task Get_SecurePageRequiresAnAuthenticatedUser(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient(new WebApplicationFactoryClientOptions {AllowAutoRedirect = false});
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.StartsWith("http://localhost/user/login",
+                response.Headers.Location.OriginalString);
+        }
+    }
+}
---
+++ MoneyManager.Tests/Utilites.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using MoneyManager.DAL;
+using MoneyManager.Models;
+
+namespace MoneyManager.IntegrationTests
+{
+    public class Utilites
+    {
+        public static void InitializeDbForTests(AppDbContext db)
+        {
+            db.Accounts.AddRange(GetSeedingMessages());
+            db.SaveChanges();
+        }
+
+        public static void ReinitializeDbForTests(AppDbContext db)
+        {
+            db.Accounts.RemoveRange(db.Accounts);
+            InitializeDbForTests(db);
+        }
+
+        private static IEnumerable<Account> GetSeedingMessages()
+        {
+            return new List<Account>
+            {
+                new Account {Id = 1, InitialBalance = 0, Name = "Account 1"},
+                new Account {Id = 2, InitialBalance = 0, Name = "Account 2"},
+                new Account {Id = 3, InitialBalance = 0, Name = "Account 3"}
+            };
+        }
+    }
+}
